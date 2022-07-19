c7bd2a27264688d876e8acdd5896bcae71164126
mkyb <mitukouyb@gmail.com>
2022-07-20 02:12:53 +0900

    HOME_PROTOTYPE

--- sp.js
+++ sp.js
@@ -24,25 +24,75 @@ jQuery(function () {
   jQuery('#box').draggable({
     axis: 'y',
     scroll: false,
+    containment: [0, -10000, 0, 0],
   });
 });
+
+function lockopen() {
+  $('#apps').addClass("active");
+  $('#dock').removeClass("hidedock");
+  const app_1 = document.getElementById("app1");
+  $('#dock').animate({
+    bottom: 0.0325 * window.innerHeight,
+  }, 500, 'swing');
+}
+
+//let boxflag = true;
+
+const boxtimer = setInterval(function () {
+  const box = document.querySelector('#box');
+  const y = box.getBoundingClientRect().top;
+  //if (y < 0) {
+  /*$('#lwp').animate({
+    opacity: "0.5",
+  }, 100, 'swing');
+  boxflag = false;*/
+  //}
+  let lwpop;
+  let yrat = -y / window.innerHeight;
+  if (yrat < 0.45) {
+    lwpop = 1 - 1 / 7 * Math.log(1 + yrat);
+  } else {
+    lwpop = 1 - 1 / 7 * Math.log(1.45);
+  }
+  document.getElementById("lwp").style.opacity = lwpop;
+  document.getElementById("hstatus").style.opacity = 1 / 2 * Math.exp(yrat) - 1 / 2;
+  if (yrat >= 0.45) {
+    lockopen();
+  }
+  if (y == 0) {
+    //document.getElementById("dock").style.bottom = "-10vh";
+    $('#dock').addClass("hidedock");
+    /*$('#lwp').animate({
+      opacity: "1",
+    }, 10, 'swing');
+    boxflag = true;*/
+    document.getElementById("lwp").style.filter = "blur(0)";
+    $('#apps').removeClass("active");
+  } else {
+    document.getElementById("lwp").style.filter = "blur(" + 80 * yrat + "px)";
+  }
+  $('.hidedock').css("bottom", "-10vh");
+}, 100);
 
 function swipeup() {
   const box = document.querySelector('#box');
   const y = box.getBoundingClientRect().top;
   if (y <= -0.2 * window.innerHeight) {
     $('#box').animate({
-      top: -10 * window.innerHeight,
-    }, 2000, 'swing');
+      top: -1 * window.innerHeight,
+    }, 500, 'easeInOutSine', function () {
+      clearInterval(boxtimer);
+    });
   } else {
     $('#box').animate({
       top: 0,
       left: 0,
       right: 0,
-      //marginLeft: auto,
-      //marginRight: auto,
       padding: 0,
-    }, 500, 'swing');
+    }, 500, 'easeOutQuart', function () {
+      //document.getElementById("dock").style.bottom = "-10vh";
+    });
   }
 }
 
@@ -61,6 +111,9 @@ function stuanime() {
     });
   }, 500);
 }
+
+
+
 
 $(window).on("orientationchange", function () {
   const modal = document.getElementById('rotmodal');
